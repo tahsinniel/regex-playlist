21340d49c44cfdfe7604b691d9d0a8b1a4848c24
tahsinniel <tahsin.niel@gmail.com>
2022-08-20 23:45:22 +0600

    email regex

--- validation.js
+++ validation.js
@@ -6,6 +6,7 @@ const patterns = {
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
     slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 
 };
 
